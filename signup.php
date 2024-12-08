ff719a0215b5af662a3d4778b58998fa2fefdebf
rick <ricksanmateo27@gmail.com>
2024-12-08 16:12:23 +0800

    08 sign up page

---
+++ signup.php
@@ -0,0 +1,24 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Signup</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="signup_wrapper">
+        <div class="signup_header">Signup</div>
+        <form action="">
+            <input type="text" name="username" placeholder="Username"><br>
+            <div class="gender-container">
+                <br>Gender<br>
+                <input type="radio" name="gender" value="male">Male<br>
+                <input type="radio" name="gender" value="female">Female<br>
+            </div>
+            <input type="password" name="password" placeholder="Password"><br>
+            <input type="password" name="password2" placeholder="Retype Password"><br>
+            <input type="submit" value="Signup"><br>
+        </form>
+    </div>
+</body>
+</html>
