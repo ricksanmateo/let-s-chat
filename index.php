086940373579b1e78d8e710c199cfc1db530277c
rick <ricksanmateo27@gmail.com>
2024-12-08 09:47:13 +0800

    01-02-03

---
+++ index.php
@@ -0,0 +1,42 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Let's Chat</title>
+    <link rel="stylesheet" href="style.css">
+    <meta name="description" content="Chat App">
+</head>
+<body>
+    <div id="wrapper">
+        <div id="left_pannel">
+            <div style="padding: 10px;">
+                <img class="profile_img" src="./images/user3.jpg" alt="">
+                <br>
+                Kelly Hartman
+                <br>
+                <span class="email_add">kellyhatman@gmail.com</span>
+                <br>
+                <br>
+                <br>
+                <div class="icons">
+                    <label for="radio_chat"><img src="./icons/chat.png" alt=""></label>
+                    <label for="radio_contacts"><img src="./icons/contacts.png" alt=""></label>
+                    <label for="radio_settings"><img src="./icons/settings.png" alt=""></label>
+            </div>
+            </div>
+            
+        </div>
+        <div id="right_pannel">
+            <div id="header">Let's Chat</div>
+            <div id="container">
+                
+                <div id="inner_left_pannel"></div>
+                <input id="radio_chat" type="radio" name="myRadio" class="myRadio">
+                <input id="radio_contacts" type="radio" name="myRadio" class="myRadio">
+                <input id="radio_settings" type="radio" name="myRadio" class="myRadio">
+                <div id="inner_right_pannel"></div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
